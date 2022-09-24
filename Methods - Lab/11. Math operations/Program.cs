75ca7fa7604044b8d998f73d004c9a8d4b7fe6d6
Anton Ivanov <90517326+TSI2901@users.noreply.github.com>
2022-09-24 14:48:16 +0300

    Add files via upload

---
+++ Methods - Lab/11. Math operations/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _11._Math_operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = double.Parse(Console.ReadLine());
+            char operation =char.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            Operation(a, operation, b);
+        }
+        static void Operation(double A, char OP, double B) 
+        {
+            double sum = 0;
+            if (OP == '*')
+            {
+                sum = A * B;
+            }
+            else if (OP == '+')
+            {
+                sum = A + B;
+            }
+            else if (OP == '-')
+            {
+                sum = A - B;
+            }
+            else if (OP == '/')
+            {
+                sum = A / B;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
