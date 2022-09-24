d894adffb6e5b5cad3bff741bf416ca22ab11876
Anton Ivanov <90517326+TSI2901@users.noreply.github.com>
2022-09-24 14:46:00 +0300

    Add files via upload

---
+++ Methods - Lab/08. Math Power/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _08._Math_Power
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double number = double.Parse(Console.ReadLine());
+            double power = double.Parse(Console.ReadLine());
+            CalculateTahtNumber(number, power);
+        }
+
+        static void CalculateTahtNumber(double b, double power) 
+        {
+            double sum = b;
+            for (int i = 1; i < power; i++)
+            {
+                sum *= b;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
