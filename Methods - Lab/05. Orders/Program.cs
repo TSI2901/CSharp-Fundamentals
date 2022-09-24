3a30946b4aaea7f7d4cfe9965723b265caaf8e01
Anton Ivanov <90517326+TSI2901@users.noreply.github.com>
2022-09-24 14:44:11 +0300

    Add files via upload

---
+++ Methods - Lab/05. Orders/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _05._Orders
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string thing = Console.ReadLine();
+            int n = int.Parse(Console.ReadLine());
+            PrintSum(thing, n);
+        }
+
+        static void PrintSum(string drink, int NumberOfDrinks) 
+        {
+            double sum = 0;
+            if (drink == "water")
+            {
+                sum = NumberOfDrinks * 1;
+            }
+            else if (drink == "coffee") 
+            {
+                sum = NumberOfDrinks * 1.50;
+            }
+            else if (drink == "coke")
+            {
+                sum = NumberOfDrinks * 1.40;
+            }
+            else if (drink == "snacks")
+            {
+                sum = NumberOfDrinks * 2;
+            }
+            Console.WriteLine($"{sum:F2}");
+        }
+    }
+}
