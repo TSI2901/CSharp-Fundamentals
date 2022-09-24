770cf9e6ed89c15635e47bf931b5e7277ad536fb
Anton Ivanov <90517326+TSI2901@users.noreply.github.com>
2022-09-24 14:45:25 +0300

    Add files via upload

---
+++ 07. Repeat String/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _07._Repeat_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            int n = int.Parse(Console.ReadLine());
+            RepeatWord(word, n);
+        }
+
+        static void RepeatWord(string word, int NumberOfRepeating) 
+        {
+            for (int i = 0; i < NumberOfRepeating; i++)
+            {
+                Console.Write(word);
+            }
+        }
+    }
+}
