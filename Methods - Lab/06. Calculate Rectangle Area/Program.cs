4467ec2d243132c641007235d9cd29c3cc95eac3
Anton Ivanov <90517326+TSI2901@users.noreply.github.com>
2022-09-24 14:44:40 +0300

    Add files via upload

---
+++ Methods - Lab/06. Calculate Rectangle Area/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _06._Calculate_Rectangle_Area
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            CalculateArea(a, b);
+        }
+
+        static void CalculateArea(int A, int B) 
+        {
+            int sum = A * B;
+            Console.WriteLine(sum);
+        }
+    }
+}
