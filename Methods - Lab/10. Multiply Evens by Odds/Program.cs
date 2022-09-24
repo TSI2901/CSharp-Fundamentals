a36261405ce5c26618f3e8639d86dcae1cf1e801
Anton Ivanov <90517326+TSI2901@users.noreply.github.com>
2022-09-24 14:47:38 +0300

    Add files via upload

---
+++ Methods - Lab/10. Multiply Evens by Odds/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _10._Multiply_Evens_by_Odds
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Sumdigits(n);
+        }
+        static void Sumdigits(int number) 
+        {
+            int LastDigit = 0;
+            int EvenSum = 0;
+            int OddSum = 0;
+            while (number != 0)
+            {
+                LastDigit = number % 10;
+                number = number / 10;
+                if (LastDigit % 2 == 0)
+                {
+                    EvenSum += LastDigit;
+                }
+                else
+                {
+                    OddSum += LastDigit;
+                }
+            }
+            Console.WriteLine(EvenSum * OddSum);
+        }
+    }
+}
