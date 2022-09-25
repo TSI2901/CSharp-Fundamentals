bba88ffb533dc97c6b77ab0d622d87a884099d55
Anton Ivanov <90517326+TSI2901@users.noreply.github.com>
2022-09-25 11:39:41 +0300

    Add files via upload

---
+++ Methods - Exercise/06. Middle Characters/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _06._Middle_Characters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Proverka(input);
+        }
+        static void Proverka(string input) 
+        {
+            int lenght = input.Length;
+            if (lenght % 2 == 0)
+            {
+                Console.Write(input[(lenght / 2) - 1]);
+                Console.Write(input[lenght / 2]);
+            }
+            else
+            {
+                Console.WriteLine(input[lenght / 2]);
+            }
+        }
+    }
+}
