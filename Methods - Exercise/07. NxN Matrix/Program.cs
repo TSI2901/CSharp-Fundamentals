fe6001f2d35abc5468a5e149cad0468b02bd622b
Anton Ivanov <90517326+TSI2901@users.noreply.github.com>
2022-09-25 11:41:02 +0300

    Add files via upload

---
+++ Methods - Exercise/07. NxN Matrix/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _07._NxN_Matrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            PrintNumbers(n);
+        }
+        static void PrintNumbers(int n) 
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int k = 0; k < n; k++)
+                {
+                    Console.Write($"{n} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
