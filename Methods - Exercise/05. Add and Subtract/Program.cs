98193cb40ba4e62efbea002fa17187f54810101c
Anton Ivanov <90517326+TSI2901@users.noreply.github.com>
2022-09-25 11:39:08 +0300

    Add files via upload

---
+++ Methods - Exercise/05. Add and Subtract/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _05._Add_and_Subtract
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            Subtract(a, b, c);
+        }
+        static void AddTwo(int a , int b) 
+        {
+            
+            int sum = a + b;
+        }
+        static void Subtract(int a, int b, int c) 
+        {
+            
+            Console.WriteLine((a + b) - c);
+        }
+    }
+}
