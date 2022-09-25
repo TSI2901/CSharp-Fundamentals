8333546c6ff16e04f00862203a2dffa6d0a18620
Anton Ivanov <90517326+TSI2901@users.noreply.github.com>
2022-09-25 11:35:28 +0300

    Add files via upload

---
+++ Methods - Exercise/01. Smallest of Three Numbers/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _01._Smallest_of_Three_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            Proverka(a, b, c);
+        }
+        static void Proverka(int a, int b, int c) 
+        {
+            int min = 0;
+            if (a < b && a < c)
+            {
+                min = a;
+            }
+            else if (b < a && b < c) 
+            {
+                min = b;
+            }
+            else if (c < a && c < b) 
+            {
+                min = c;
+            }
+            else
+            {
+                min = a;
+            }
+            Console.WriteLine(min);
+        }
+    }
+}
