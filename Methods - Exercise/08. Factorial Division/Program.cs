a2f3055045393efb98a453000103c3e2b7c0a792
Anton Ivanov <90517326+TSI2901@users.noreply.github.com>
2022-09-25 11:41:51 +0300

    Add files via upload

---
+++ Methods - Exercise/08. Factorial Division/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _08._Factorial_Division
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            Factoriel(a, b);
+        }
+        static void Factoriel(double a, double b) 
+        {
+            double allA = a;
+            double allB = b;
+            for (double i = a-1; i > 0; i--)
+            {
+                allA *= i;
+            }
+            for (double i = b - 1; i > 0; i--)
+            {
+                allB *= i;
+            }
+            double sum = allA / allB;
+            Console.WriteLine($"{sum:f2}");
+        }
+    }
+}
