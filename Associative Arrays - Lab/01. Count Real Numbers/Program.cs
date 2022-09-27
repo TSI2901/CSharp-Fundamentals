26ed88d573cf31ae6f7af8d8ab91d5090c1addc9
Anton Ivanov <90517326+TSI2901@users.noreply.github.com>
2022-09-27 17:15:21 +0300

    Add files via upload

---
+++ Associative Arrays - Lab/01. Count Real Numbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Count_Real_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+            SortedDictionary<int, int> stoinost = new SortedDictionary<int, int>();
+            foreach (int number in numbers) 
+            {
+                if (stoinost.ContainsKey(number))
+                {
+                    stoinost[number] += 1;
+                }
+                else
+                {
+                    stoinost.Add(number, 1);
+                }
+            }
+            foreach (var item in stoinost)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
