7ab3d43763b8a616c1b5aeac1da8ba3bc592aa8c
Anton Ivanov <90517326+TSI2901@users.noreply.github.com>
2022-09-27 17:42:32 +0300

    Add files via upload

---
+++ Associative Arrays - Lab/02. Odd Occurrences/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Odd_Occurrences
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split(' ');
+            Dictionary<string, int> counts = new Dictionary<string, int>();
+
+            foreach (string word in words) 
+            {
+                string currentWord = word.ToLower();
+                if (counts.ContainsKey(currentWord))
+                {
+                    counts[currentWord]++;
+                }
+                else
+                {
+                    counts.Add(currentWord, 1);
+                }
+            }
+            foreach (var count in counts) 
+            {
+                if (count.Value % 2 != 0)
+                {
+                    Console.Write(count.Key + " ");
+                }
+            }
+        }
+    }
+}
