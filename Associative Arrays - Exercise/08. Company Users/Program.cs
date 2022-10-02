5cb082e335762a826d98a54e6cd11939249f1845
Anton Ivanov <90517326+TSI2901@users.noreply.github.com>
2022-10-02 15:51:59 +0300

    Add files via upload

---
+++ Associative Arrays - Exercise/08. Company Users/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _08._Company_Users
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> map = new Dictionary<string, List<string>>();
+            string[] command = Console.ReadLine().Split(" -> ");
+            while (command[0] != "End") 
+            {
+                if (!map.ContainsKey(command[0]))
+                {
+                    List<string> list = new List<string>();
+                    list.Add(command[1]);
+                    map.Add(command[0], list);
+                }
+                if (!map[command[0]].Contains(command[1]))
+                {
+                    map[command[0]].Add(command[1]);
+                }
+                //else
+                //{
+                //    List<string>  sss = map[command[0]];
+                //    foreach (var value in sss)
+                //    {
+                //        if (value == command[1])
+                //        {
+                            
+                //            break;
+                //        }
+                //        else
+                //        {
+                //            map[command[0]].Add(command[1]);
+                //            break;
+                //        }
+                //    }
+                //}
+                command = Console.ReadLine().Split(" -> ");
+            }
+            foreach (var item in map) 
+            {
+                Console.WriteLine(item.Key);
+                foreach (var value in item.Value) 
+                {
+                    Console.WriteLine($"-- {value}");
+                }
+            }
+        }
+    }
+}
