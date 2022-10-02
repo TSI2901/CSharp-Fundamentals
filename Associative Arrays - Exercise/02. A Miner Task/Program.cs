e6071e94f4413986ecd5e7c4cb983c7693b9ac10
Anton Ivanov <90517326+TSI2901@users.noreply.github.com>
2022-10-02 15:49:24 +0300

    Add files via upload

---
+++ Associative Arrays - Exercise/02. A Miner Task/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _02._A_Miner_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> mina = new Dictionary<string, int>();
+            string vid = Console.ReadLine();
+            while (vid != "stop") 
+            {
+                int num = int.Parse(Console.ReadLine());
+                if (mina.ContainsKey(vid))
+                {
+                    mina[vid] += num;
+                }
+                else
+                {
+                    mina.Add(vid, num);
+                }
+                vid = Console.ReadLine();
+            }
+            foreach (var item in mina) 
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
